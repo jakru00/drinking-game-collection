72c2b002642ed7748730a9496fc549de81a39227
Jannis Krumbholz <jannis.krumbholzt2informatik.de>
2024-05-18 11:07:17 +0200

    3: delegated inner routing to piccolo game

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
-import { PiccoloRootComponentComponent } from '../piccolo/root/piccolo-root-component/piccolo-root-component.component';
 
 export const routes: Routes = [
     {
-        path: '', title: 'Piccolo', component: PiccoloRootComponentComponent,
+        path: '',
+        title: 'Piccolo',
+        loadChildren: () => 
+          import('../piccolo/root/piccolo-routes')
+          .then((piccoloGame) => piccoloGame.PICCOLO_ROUTES)
     },
     { path: '**', redirectTo: '', },
 ];
---
+++ src/piccolo/root/piccolo-routes.ts
@@ -0,0 +1,8 @@
+import { Route } from "@angular/router";
+import { PiccoloRootComponentComponent } from "./piccolo-root-component/piccolo-root-component.component";
+
+export const PICCOLO_ROUTES: Route[] = [
+    {
+        path: '', component: PiccoloRootComponentComponent
+    }
+]

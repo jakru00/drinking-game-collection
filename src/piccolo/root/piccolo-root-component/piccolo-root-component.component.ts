a302adbe757fd5fb433a3d7b8f5fe598f7303b1c
Jannis Krumbholz <jannis.krumbholzt2informatik.de>
2024-05-18 11:12:00 +0200

    3: added start page component

--- src/piccolo/root/piccolo-root-component/piccolo-root-component.component.ts
+++ src/piccolo/root/piccolo-root-component/piccolo-root-component.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { StartPageComponent } from '../../features/start-page/start-page/start-page.component';
 
 @Component({
   selector: 'app-piccolo-root-component',
   standalone: true,
-  imports: [],
+  imports: [StartPageComponent],
   templateUrl: './piccolo-root-component.component.html',
 })
 export class PiccoloRootComponentComponent {

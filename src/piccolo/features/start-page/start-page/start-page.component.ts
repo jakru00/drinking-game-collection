8ea10d2f58e9bbe7fae6036349d7c166b41afb09
Jannis Krumbholz <jannis.krumbholzt2informatik.de>
2024-05-18 12:47:24 +0200

    3: created start page

--- src/piccolo/features/start-page/start-page/start-page.component.ts
+++ src/piccolo/features/start-page/start-page/start-page.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from "@angular/material/button";
 
 @Component({
   selector: 'piccolo-start-page',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, MatButtonModule],
   templateUrl: './start-page.component.html',
   styleUrl: './start-page.component.scss'
 })

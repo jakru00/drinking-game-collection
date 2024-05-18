e94401a37ee18314de51efc64ac7927178c60706
Jannis Krumbholz <jannis.krumbholzt2informatik.de>
2024-05-18 10:51:55 +0200

    2: removed dashboard and dashboard route as only one game exists so far

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,9 @@
 import { Routes } from '@angular/router';
 import { PiccoloRootComponentComponent } from '../piccolo/root/piccolo-root-component/piccolo-root-component.component';
-import { AppComponent } from './app.component';
-import { DashboardRootComponent } from '../dashboard/root/dashboard-root/dashboard-root.component';
 
 export const routes: Routes = [
     {
-        path: 'piccolo', title: 'Piccolo', component: PiccoloRootComponentComponent,
-    },
-    {
-        path: '', title: 'Drinking Game Collection', component: DashboardRootComponent,
+        path: '', title: 'Piccolo', component: PiccoloRootComponentComponent,
     },
     { path: '**', redirectTo: '', },
 ];

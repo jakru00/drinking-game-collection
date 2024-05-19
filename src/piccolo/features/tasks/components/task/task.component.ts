ddb2a4be5e99b0f30d16ca9142c4c984e22959cc
Jannis Krumbholz <jannis.krumbholzt2informatik.de>
2024-05-19 21:14:29 +0200

    7: add first styling to task component

--- src/piccolo/features/tasks/components/task/task.component.ts
+++ src/piccolo/features/tasks/components/task/task.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './task.component.html',
   styleUrl: './task.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush

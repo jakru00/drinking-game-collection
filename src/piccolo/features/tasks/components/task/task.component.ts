84269b017099b9fd52d89e1d2e5ab5bf7c73bb4b
Jannis Krumbholz <jannis.krumbholzt2informatik.de>
2024-05-22 13:10:17 +0200

    7: WIP

--- src/piccolo/features/tasks/components/task/task.component.ts
+++ src/piccolo/features/tasks/components/task/task.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, MatButtonModule],
   templateUrl: './task.component.html',
   styleUrl: './task.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush

bbaf8cff86f110eec421036b69651b6eec00168f
jakru00 <jakru00@gmx.de>
2024-05-22 16:56:22 +0200

    19: focus input on add player

--- src/piccolo/components/player/player.component.ts
+++ src/piccolo/components/player/player.component.ts
@@ -23,7 +23,7 @@ export class PlayerComponent {
     if(input.value !== '') {
       this.players.push(input.value);
       input.value = '';
-      input.blur();
+      input.focus();
     }
   }
 

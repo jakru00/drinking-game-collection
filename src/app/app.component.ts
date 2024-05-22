1a1d919e567d7a24fae6406105f002087f4a688d
jakru00 <jakru00@gmx.de>
2024-05-22 14:55:34 +0200

    15: ensure user is navigated to root page on reload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +10,8 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'drinking-game-collection';
+
+  constructor(private readonly router: Router) {
+    router.navigateByUrl('');
+  }
 }

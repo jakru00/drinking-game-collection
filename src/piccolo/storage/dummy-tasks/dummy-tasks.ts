72ecdce32b18aae9ad12bd921ed612443a4f194a
jakru00 <jakru00@gmx.de>
2024-05-22 15:56:46 +0200

    15: prettify dummy tasks

--- src/piccolo/storage/dummy-tasks/dummy-tasks.ts
+++ src/piccolo/storage/dummy-tasks/dummy-tasks.ts
@@ -1,48 +1,44 @@
 import { Task } from "../task.type";
 
-export const DUMMY_TASKS: Task[] = [
+export const DUMMY_TASKS: ReadonlyArray<Task> = [
     {
         title: 'Dummy task 1',
         description: 'dummy description 1'
-    },
-    {
-        title: 'Dummy task 1',
-        description: 'dummy description 2'
     },
     {
         title: 'Dummy task 2',
-        description: 'dummy description 3'
+        description: 'dummy description 2'
     },
     {
         title: 'Dummy task 3',
-        description: 'dummy description 4'
+        description: 'dummy description 3'
     },
     {
         title: 'Dummy task 4',
-        description: 'dummy description 5'
+        description: 'dummy description 4'
     },
     {
         title: 'Dummy task 5',
-        description: 'dummy description 6'
+        description: 'dummy description 5'
     },
     {
         title: 'Dummy task 6',
-        description: 'dummy description 7'
+        description: 'dummy description 6'
     },
     {
         title: 'Dummy task 7',
-        description: 'dummy description 8'
+        description: 'dummy description 7'
     },
     {
         title: 'Dummy task 8',
-        description: 'dummy description 9'
+        description: 'dummy description 8'
     },
     {
         title: 'Dummy task 9',
-        description: 'dummy description 10'
+        description: 'dummy description 9'
     },
     {
         title: 'Dummy task 10',
-        description: 'dummy description 1'
+        description: 'dummy description 10'
     },
 ]
